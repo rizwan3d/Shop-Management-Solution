33ff17e1c3594af41afc2fa71946ab099c8d1a04
naveedanjum <naveedanjum@f4af5a58-50bf-49b6-9025-600ccb893d9c>
2012-04-04 19:46:35 +0000

    Added DAL files for Vendor / Contractor
    1. XtraTreeDAL
    2. XtraNodeType
    3. XtraTreeNode

---
+++ Shop Management Solution/lib/XtraTreeNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop_Management_Solution.lib
+{
+    class XtraTreeNode
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        private string mobileNo;
+
+        public string MobileNo
+        {
+            get { return mobileNo; }
+            set { mobileNo = value; }
+        }
+        private string contactNo;
+
+        public string ContactNo
+        {
+            get { return contactNo; }
+            set { contactNo = value; }
+        }
+        private string location;
+
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        private XtraTreeNode parent;
+
+        internal XtraTreeNode Parent
+        {
+            get { return parent; }
+            set { parent = value; }
+        }
+
+        private XtraNodeType nodeType;
+
+        internal XtraNodeType NodeType
+        {
+            get { return nodeType; }
+            set { nodeType = value; }
+        }
+
+
+
+    }
+}
---
+++ Shop Management Solution/lib/dal/XtraTreeDAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using System.Data;
+using Shop_Management_Solution.lib.util;
+
+namespace Shop_Management_Solution.lib.dal
+{
+    class XtraTreeDAL
+    {
+        public bool InsertXtraTreeNode(XtraTreeNode node)
+        {
+            //Create the objects we need to insert a new record
+            OleDbConnection cnInsert = new OleDbConnection(DBUtil.GetConnectionString());
+            OleDbCommand cmdInsert = new OleDbCommand();
+            string query = "INSERT INTO Xtra_Tree(Name,Parent_ID,Type,Email,Mobile_No,Contact_No,Location,Is_Deleted)" + 
+                            "VALUES(@name,@parentId,@type,@email,@mobile,@contactNo,@location,0)";
+            
+            int iSqlStatus;
+            cmdInsert.Parameters.Clear();
+
+            try
+            {
+                cmdInsert.CommandText = query;
+                cmdInsert.CommandType = CommandType.Text;
+
+                cmdInsert.Parameters.AddWithValue("@name", node.Name);
+                cmdInsert.Parameters.AddWithValue("@parentId", node.Parent.Id);
+                cmdInsert.Parameters.AddWithValue("@type",node.NodeType.Name );
+                cmdInsert.Parameters.AddWithValue("@email", node.Email);
+                cmdInsert.Parameters.AddWithValue("@mobile", node.MobileNo);
+                cmdInsert.Parameters.AddWithValue("@contactNo", node.ContactNo);
+                cmdInsert.Parameters.AddWithValue("@location", node.Location);
+
+                cmdInsert.Connection = cnInsert;
+                DBUtil.HandleConnection(cnInsert);
+                iSqlStatus = cmdInsert.ExecuteNonQuery();
+
+                //Now check the status
+                if (iSqlStatus == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                //Now close the connection
+                DBUtil.HandleConnection(cnInsert);
+            }
+        }
+    }
+}
